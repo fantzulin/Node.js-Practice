84d00c76af0cb6f508d5fd131486a01adbb8f320
fantzulin <fanchi0917@gmail.com>
2019-08-18 20:40:12 +0800

    add cost function
    update index page view, add create page addCost Function

--- node-irich/routes/index.js
+++ node-irich/routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'iRich 愛記帳'
-  });
+  models.Account.findAll().then(function (accounts) {
+    res.render('index', {
+      title: 'iRich 愛記帳',
+      accounts: accounts
+    });
+  })
 });
 
 
